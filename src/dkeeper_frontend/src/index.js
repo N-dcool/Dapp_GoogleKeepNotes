6869a3f2f687488edf924eff6caf5b0694a096d8
Cnaresh02 <nareshchoudhary@NARESHs-MacBook-Pro.local>
2023-01-16 13:41:50 +0530

    Initial commit.

---
+++ src/dkeeper_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { dkeeper_backend } from "../../declarations/dkeeper_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await dkeeper_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
